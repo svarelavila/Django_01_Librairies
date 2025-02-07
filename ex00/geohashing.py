6bf013228d0410db9b6317f5e5fe22fca9eea0e6
Tu Nombre <tu@email.com>
2025-02-07 18:20:59 +0100

    modulo 01 lib

--- ex00/geohashing.py
+++ ex00/geohashing.py
@@ -1,28 +1,35 @@
 import sys
 import antigravity
 
-def calculate_geohash():
+
+def geohash():
+    """
+    calcula y abre una ubicación geohasheada en Google Maps usando `antigravity`.  
+    Argumentos: latitud, longitud y fecha.
+    """
     # Validar número de argumentos
     if len(sys.argv) != 4:
-        print("Usage: python geohashing.py <latitude> <longitude> <date>", file=sys.stderr)
+        print("Usage: python geohashing.py <latitude> <longitude> <date>")
+        sys.exit(1)  # Importante para que el script no continúe ejecutándose
 
     try:
-        #Extraer y convertir argumentos
+        # Extraer y convertir argumentos
         latitude = float(sys.argv[1])
         longitude = float(sys.argv[2])
-        date = sys.argv[3].encode() # Codificar la fecha como bytes para antigravity
+        date = sys.argv[3].encode()  # Codificar la fecha como bytes para antigravity
 
-        #Calcular el geohash
+        # Calcular el geohash
         antigravity.geohash(latitude, longitude, date)
 
     except ValueError as e:
         # Manejo de errores de conversión
-        print(f"Error: Invalid input fomat. {e}", file=sys.stderr)
+        print(f"Error: Invalid input format. {e}")
         sys.exit(1)
     except Exception as e:
         # Manejo de cualquier otro error inesperado
-        print(f"An unexpected error ocurred: {e}", file=sys.stderr)
+        print(f"An unexpected error occurred: {e}")
         sys.exit(1)
 
+
 if __name__ == "__main__":
-    calculate_geohash()
+    geohash()
--- ex01/my_program.py
+++ ex01/my_program.py
@@ -4,18 +4,20 @@ import sys
 sys.path.insert(0, "local_lib")
 from local_lib.path import Path
 
+
 def main():
     # Crear el directorio my_folder
     folder = Path("my_folder")
-    folder.mkdir_p() # Crear carpeta, incluso si ya existe
+    folder.mkdir_p()  # Crear carpeta, incluso si ya existe
 
     # Crear un archivo dentro de my_folder
     file = folder / "my_file.txt"
-    file.write_text("Hello, World!!!!!!!!!")
+    file.write_text("¡Hola, este es un archivo creado con path.py!\n")
 
     # Leer el contenido del archivo y mostrarlo
     content = file.read_text()
     print(f"File content: {content}")
+
 
 if __name__ == "__main__":
     main()
